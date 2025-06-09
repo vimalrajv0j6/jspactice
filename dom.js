17f62b1f5eb2efde9375647004f5c70c6f5772ac
vimalrajv0j6 <vimalrajv0r6@gmail.com>
2025-06-09 18:56:28 +0530

    DOM js

---
+++ dom.js
@@ -0,0 +1,12 @@
+let view1 = document.getElementById("view1");
+console.log(view1);
+const view2 = document.querySelector("#view2");
+console.log(view2);
+const views = document.getElementsByClassName("view");
+const sameviews = document.querySelectorAll(".view")
+console.log(views);
+console.log(sameviews);
+const divs = view1.querySelectorAll("div");
+console.log(divs);
+const sameDiv = view1.getElementsByTagName("div");
+console.log(sameDiv);
