def425072e659f82f2e82952dadb076f5d1711fa
vimalrajv0j6 <vimalrajv0r6@gmail.com>
2025-06-13 11:57:01 +0530

    count app

---
+++ counter app/index.js
@@ -0,0 +1,18 @@
+var increment = document.getElementById('increment');
+var decrement = document.getElementById('decrement');
+var reset = document.getElementById('reset');
+var count = document.getElementById('count');
+let countNumber=0;
+
+increment.addEventListener('click',()=>{
+   countNumber++;
+   count.textContent=countNumber;
+})
+decrement.addEventListener('click',()=>{
+   countNumber--;
+   count.textContent=countNumber;
+})
+reset.addEventListener('click',()=>{
+   countNumber=0;
+   count.textContent=countNumber;
+})
